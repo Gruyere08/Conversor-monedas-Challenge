5899715bf74b96be7e9a66801230b70de3423b73
Jacob <athiaguin.es@gmail.com>
2024-10-03 18:39:28 -0300

    Menus finalizados

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,21 @@
 import Modelos.AyudanteHttp;
 import Modelos.ConversorDeMonedas;
+import Modelos.SelectorDePais;
 import Modelos.TablaDeConversion;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int opcion = 0;
+
         Scanner teclado = new Scanner(System.in);
+        String monedaInicial = "";
+        String monedaFinal = "";
+        double monto = 0;
 
 
         System.out.println("###########Conversor de monedas###########");
-        System.out.println("Especifique la moneda que desea ingresar");
         String menu = """
                 1. Peso argentino
                 2. Boliviano
@@ -20,27 +24,104 @@ public class Main {
                 5. Peso Colombiano
                 6. Dolar EstadoUnidense
                 7. salir
-                Tu opcion: 
-                """;
-        while(true){
-            System.out.println(menu);
-            opcion = teclado.nextInt();
-            if (opcion == 7){
-                System.exit(0);
-            } else {
+                Tu opcion: """;
 
+
+        //Ciclo operativo de la moneda inicial
+        while(true){
+            System.out.println("Especifique la moneda que desea ingresar");
+            System.out.println(menu);
+            int opcion = 0;
+            try{
+                opcion = teclado.nextInt();
+                if (opcion == 7){
+                    System.out.println("Terminando el programa");
+                    System.exit(0);
+                } else {
+                    monedaInicial = SelectorDePais.obtenerPais(opcion);
+                    if (monedaInicial.equals("XXX")){
+                        System.out.println("La opción seleccionada no es valida, intentelo de nuevo");
+                        System.out.println("######################################################");
+                    } else {
+                        break;
+                    }
+                }
+            }catch(InputMismatchException e){
+                System.out.println("La opción seleccionada no es valida, intentelo de nuevo");
+                System.out.println("######################################################");
+                teclado.next();
             }
+
+
         }
-        String monedaInicial = teclado.nextLine();
-        String monedaFinal = "usd";
-        double monto = 0;
-        try {
-            ConversorDeMonedas.convertirMoneda(monedaInicial,monedaFinal,monto);
-        } catch (NoSuchFieldException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
+
+        //Ciclo operativo del monto
+        while(true){
+            System.out.println("######################################################");
+            System.out.println("Ingrese el monto que desea convertir");
+            System.out.println("* Use puntos para separar los decimales");
+            System.out.println("Su monto: ");
+
+            try{
+                monto = teclado.nextDouble();
+                if(monto <= 0){
+                    System.out.println();
+                } else {
+                    break;
+                }
+            }catch (InputMismatchException e){
+                System.out.println("El Valor ingresado no es válido");
+                System.out.println("* Recuerde usar puntos para separar los decimales");
+                teclado.next();
+            }
+
+
         }
+
+
+        //Ciclo operativo del monto final
+        while(true){
+            System.out.println("Especifique a que moneda desea convertir el monto");
+            System.out.println(menu);
+            int opcion = 0;
+            try{
+                opcion = teclado.nextInt();
+                if (opcion == 7){
+                    System.out.println("Terminando el programa");
+                    System.exit(0);
+                } else {
+                    monedaFinal = SelectorDePais.obtenerPais(opcion);
+                    if (monedaFinal.equals("XXX")){
+                        System.out.println("La opción seleccionada no es valida, intentelo de nuevo");
+                        System.out.println("######################################################");
+                    } else {
+                        break;
+                    }
+                }
+            }catch(InputMismatchException e){
+                System.out.println("La opción seleccionada no es valida, intentelo de nuevo");
+                System.out.println("######################################################");
+                teclado.next();
+            }
+
+
+        }
+
+        System.out.println("Moneda Inicial: " + monedaInicial);
+        System.out.println("Moneda Final: " + monedaFinal);
+        System.out.println("Monto: " + monto);
+
+
+
+
+
+//        try {
+//            ConversorDeMonedas.convertirMoneda(monedaInicial,monedaFinal,monto);
+//        } catch (NoSuchFieldException e) {
+//            throw new RuntimeException(e);
+//        } catch (IllegalAccessException e) {
+//            throw new RuntimeException(e);
+//        }
 
         //TablaDeConversion tablaDeConversion = AyudanteHttp.pedirTabla("https://v6.exchangerate-api.com/v6/a9aeaee58a78bcadcb370047/latest/USD");
     }
